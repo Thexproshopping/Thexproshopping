756ae04472f48071909e64d21cdaaa11d8668a7c
Thexproshopping <thexproshopping@gmail.com>
2024-12-13 10:23:41 +0530

    Update order-management.js

--- scripts/order-management.js
+++ scripts/order-management.js
@@ -1,24 +1,12 @@
-function validateForm(inputs) {
-    for (let input of inputs) {
-        if (!input.value) {
-            throw new Error(`The field ${input.name} cannot be empty.`);
-        }
-    }
-}
-
-async function saveOrderData(orderData) {
+// Send Order Confirmation Email with EmailJS
+async function sendOrderConfirmation(orderData) {
     try {
-        const response = await fetch('data/orders.json');
-        const orders = await response.json();
-        orders.push(orderData);
-        await fetch('data/orders.json', {
-            method: 'POST',
-            body: JSON.stringify(orders),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        const response = await emailjs.send("service_qo8786l", "template_109i6mr", orderData);
+        if (response.status !== 200) {
+            throw new Error("Failed to send email.");
+        }
     } catch (error) {
-        console.error("Error saving order data: ", error);
+        console.error("EmailJS Error: ", error);
+        alert("There was an error sending your order confirmation. Please try again.");
     }
 }
